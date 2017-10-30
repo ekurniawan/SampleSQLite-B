78c743bf4392d3b8ff81469795fc28eea2093dda
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-30 12:08:32 +0700

    first commit

---
+++ app/src/main/java/models/MyWish.java
@@ -0,0 +1,44 @@
+package models;
+
+/**
+ * Created by erick on 30/10/2017.
+ */
+
+public class MyWish {
+    private int itemId;
+    private String title;
+    private String content;
+    private String recordDate;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRecordDate() {
+        return recordDate;
+    }
+
+    public void setRecordDate(String recordDate) {
+        this.recordDate = recordDate;
+    }
+}

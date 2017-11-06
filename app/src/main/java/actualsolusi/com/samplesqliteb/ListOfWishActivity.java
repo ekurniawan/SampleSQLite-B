4c9cbb4d7c04fb0e8cb9b9226eda9a59bd0cb17c
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-06 12:37:56 +0700

    menambahkan item decorator

--- app/src/main/java/actualsolusi/com/samplesqliteb/ListOfWishActivity.java
+++ app/src/main/java/actualsolusi/com/samplesqliteb/ListOfWishActivity.java
@@ -2,12 +2,16 @@ package actualsolusi.com.samplesqliteb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class ListOfWishActivity extends AppCompatActivity {
+    private RecyclerView rvWish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_wish);
+
+        rvWish = (RecyclerView)findViewById(R.id.rvWish);
     }
 }

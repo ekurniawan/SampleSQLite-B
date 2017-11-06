1262a5e1a897ab0df691c9e97c8afaa6ba0ea645
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-06 12:52:06 +0700

    menambahkan adapater

--- app/src/main/java/actualsolusi/com/samplesqliteb/ListOfWishActivity.java
+++ app/src/main/java/actualsolusi/com/samplesqliteb/ListOfWishActivity.java
@@ -1,11 +1,30 @@
 package actualsolusi.com.samplesqliteb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import data.DatabaseHandler;
+import models.MyWish;
 
 public class ListOfWishActivity extends AppCompatActivity {
     private RecyclerView rvWish;
+    private ArrayList<MyWish> arrListWish;
+    private DatabaseHandler db;
+
+    public ListOfWishActivity(){
+        db = new DatabaseHandler(ListOfWishActivity.this);
+    }
+
+    private void RefreshDataWish(){
+        arrListWish = db.GetAllWish();
+        WishAdapter adapter = new WishAdapter(arrListWish);
+        rvWish.setAdapter(adapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +32,10 @@ public class ListOfWishActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_of_wish);
 
         rvWish = (RecyclerView)findViewById(R.id.rvWish);
+        rvWish.addItemDecoration(new DividerItemDecoration(ContextCompat.getDrawable(
+                getApplicationContext(),R.drawable.item_decorator)));
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rvWish.setLayoutManager(llm);
     }
 }

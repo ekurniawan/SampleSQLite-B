66c88eddeaff838fcf466c8a2fddf49b9b888cb6
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-12-04 13:11:00 +0700

    menambahkan services

--- app/src/main/java/actualsolusi/com/samplesqliteb/SampleServicesActivity.java
+++ app/src/main/java/actualsolusi/com/samplesqliteb/SampleServicesActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,7 +18,9 @@ import models.Kategori;
 import services.KategoriServices;
 
 public class SampleServicesActivity extends AppCompatActivity {
-    private Button btnGetData;
+    private Button btnGetData,btnTambah;
+    private EditText etKategoriID;
+    private EditText etNamaKategori;
     private List<Kategori> listKategori;
 
     @Override
@@ -31,6 +36,44 @@ public class SampleServicesActivity extends AppCompatActivity {
                 LoadContent();
             }
         });
+
+        etNamaKategori = (EditText)findViewById(R.id.etNamaKategori);
+        btnTambah = (Button)findViewById(R.id.btnTambah);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TambahData();
+            }
+        });
+    }
+
+    private void TambahData(){
+        new AsyncTask<Void,Void,Void>(){
+            private Kategori newKategori;
+            @Override
+            protected void onPreExecute() {
+                newKategori = new Kategori();
+                newKategori.setNamaKategori(etNamaKategori.getText().toString());
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    KategoriServices.TambahKategori(newKategori);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                Toast.makeText(getApplicationContext(),"Tambah data berhasil !",Toast.LENGTH_LONG).show();
+            }
+        }.execute();
     }
 
     private void LoadContent(){
